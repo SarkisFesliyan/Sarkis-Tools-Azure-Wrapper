3fdf32af779226c1c4df2cdd394fc7eec399e20e
Sarkis Fesliyan <sarkisfesliyan@gmail.com>
2022-01-10 18:48:08 -0800

    Initial commit

---
+++ Azure.py
@@ -0,0 +1,273 @@
+import requests
+import msal
+
+
+class Azure:
+
+    def __init__(self, tenant_id, client_id, secret):
+        self.__tenant_id = tenant_id
+        self.__client_id = client_id
+        self.__secret = secret
+        self.__header = None
+        self.user_fields = '&$select=accountEnabled,ageGroup,assignedLicenses,assignedPlans,assignedPlans,' \
+                           'assignedPlans,businessPhones,city,companyName,consentProvidedForMinor,country,' \
+                           'createdDateTime,createdObjects,creationType,department,displayName,employeeHireDate,' \
+                           'employeeId,employeeOrgData,costCenter,employeeOrgData,division,employeeType,' \
+                           'externalUserState,faxNumber,givenName,identities,imAddresses,isResourceAccount,' \
+                           'jobTitle,mail,mailNickname,manager,id,mobilePhone,officeLocation,' \
+                           'onPremisesExtensionAttributes,extensionAttribute,onPremisesImmutableId,' \
+                           'onPremisesLastSyncDateTime,onPremisesProvisioningErrors,onPremisesProvisioningErrors,' \
+                           'onPremisesSamAccountName,onPremisesSecurityIdentifier,onPremisesSyncEnabled,otherMails,' \
+                           'passwordPolicies,passwordProfile,forceChangePasswordNextSignIn,passwordProfile,' \
+                           'forceChangePasswordNextSignInWithMfa,postalCode,preferredLanguage,provisionedPlans,' \
+                           'provisionedPlans,proxyAddresses,showInAddressList,state,streetAddress,surname,' \
+                           'usageLocation,userPrincipalName,userType,region'
+        self.group_fields = None
+
+    def auth(self,
+             scope=None,
+             authority=None):
+
+        if not authority:
+            authority = 'https://login.microsoftonline.com/' + self.__tenant_id
+        if not scope:
+            scope = ["https://graph.microsoft.com/.default"]
+
+        app = msal.ConfidentialClientApplication(self.__client_id, authority=authority, client_credential=self.__secret)
+        result = app.acquire_token_for_client(scopes=scope)
+
+        if "access_token" in result:
+            self.__header = {'Authorization': 'Bearer ' + result['access_token']}
+            return result
+        else:
+            print(result.get("error"))
+            print(result.get("error_description"))
+            print(result.get("correlation_id"))  # You may need this when reporting a bug
+
+            return (result.get("error") + "\n" +
+                    result.get("error_description") + "\n" +
+                    result.get("correlation_id"))
+
+    def _set_header(self, header=None):
+        if not header:
+            header = self.__header
+        else:
+            if "Authorization" not in header:
+                if self.__header:
+                    if 'Authorization' in self.__header:
+                        header['Authorization'] = self.__header['Authorization']
+        return header
+
+
+    def get_all_users(self, disabled_users=False, header=None):
+        url = 'https://graph.microsoft.com/v1.0/users?' + self.user_fields
+
+        header = self._set_header(header)
+
+        all_users = []
+
+        # Make initial request
+        response = requests.get(url, headers=header).json()
+
+        # while response is not None
+        print("Retrieved / Total")
+        while response:
+            # add to all users above
+            # print(response['value'])
+            print(len(response['value']), "/", len(all_users))
+            all_users += response['value']
+            # ask for next request if the request has nextlink in return. if there was no else, it will return none in
+            # the next while loop and exit
+            response = requests.get(response['@odata.nextLink'],
+                                    headers=header).json() if '@odata.nextLink' in response else None
+
+        if disabled_users:
+            all_users += self.get_all_disabled_users()
+
+        return all_users
+
+    def get_all_disabled_users(self, header=None):
+        url = 'https://graph.microsoft.com/v1.0/directory/deletedItems/microsoft.graph.user?' + self.user_fields
+
+        header = self._set_header(header)
+
+        all_users = []
+
+        # Make initial request
+        response = requests.get(url, headers=header).json()
+
+        # while response is not None
+        while response:
+            # add to all users above
+            print(response['value'])
+            print(len(all_users))
+            all_users += response['value']
+            # ask for next request if the request has nextlink in return. if there was no else, it will return none in
+            # the next while loop and exit
+            response = requests.get(response['@odata.nextLink'],
+                                    headers=header).json() if '@odata.nextLink' in response else None
+
+        return all_users
+
+    def get_all_groups(self, header=None):
+        url = "https://graph.microsoft.com/v1.0/groups"
+        header = self._set_header(header)
+        all_users = []
+        # Make initial request
+        response = requests.get(url, headers=header).json()
+        # while response is not None
+        while response:
+            # add to all users above
+            print(response['value'])
+            print(len(all_users))
+            all_users += response['value']
+            # ask for next request if the request has nextlink in return. if there was no else, it will return none in
+            # the next while loop and exit
+            response = requests.get(response['@odata.nextLink'],
+                                    headers=header).json() if '@odata.nextLink' in response else None
+        return all_users
+
+    def get_displayname(self, displayname, exact_match=True, memberof=False, header=None):
+        new_displayname = displayname.replace(" ", '%20')
+        url = 'https://graph.microsoft.com/v1.0/users?$count=true&$search=%22displayName:{name}%22'.format(
+            name=new_displayname) + self.user_fields
+
+        header = self._set_header(header)
+        # required for this url
+        header['ConsistencyLevel'] = 'Eventual'
+
+        # Make initial request
+        response = requests.get(url, headers=header).json()
+        # check if its EXACT value
+        if 'value' in response:
+            # check if its EXACT value
+            if exact_match:
+                found_flag = False
+                for item in response['value']:
+                    if item['displayName'].lower() == displayname.lower():
+                        found_flag = True
+                        response['value'] = [item]
+                        break
+                if not found_flag:
+                    response['value'] = []
+                    return response
+
+
+            # Go through all results
+            if memberof:
+                for num in range(len(response['value'])):
+                    if 'id' in response['value'][num]:
+                        # set empty list inside the users earlier response
+                        response['value'][num]['memberOf'] = []
+
+                        url = 'https://graph.microsoft.com/v1.0/users/{user_id}/memberOf?$count=true&'.format(
+                            user_id=response['value'][num]['id'])
+
+                        user_response = requests.get(url, headers=header).json()
+
+                        while user_response:
+                            # add to all users above
+                            response['value'][num]['memberOf'] += user_response['value']
+                            # ask for next request if the request has nextlink in return. if there was no else,
+                            # it will return none in
+                            # the next while loop and exit
+                            user_response = requests.get(user_response['@odata.nextLink'],
+                                                         headers=header).json() \
+                                if '@odata.nextLink' in user_response \
+                                else None
+        return response
+
+    def get_username(self, username, exact_match=True, memberof=False, header=None):
+        url = 'https://graph.microsoft.com/v1.0/users?$count=true&$search=' \
+              '%22userPrincipalName:{name}%22'.format(name=username) + self.user_fields
+
+        header = self._set_header(header)
+        # Required for this url
+        header['ConsistencyLevel'] = 'Eventual'
+
+        # Make initial request
+        response = requests.get(url, headers=header).json()
+        # check if its EXACT value
+        if 'value' in response:
+            # check if its EXACT value
+            if exact_match:
+                found_flag = False
+                for item in response['value']:
+                    if item['userPrincipalName'].lower().split("@")[0] == username.lower():
+                        found_flag = True
+                        response['value'] = [item]
+                        break
+                if not found_flag:
+                    response['value'] = []
+                    return response
+
+
+            # Go through all results
+            if memberof:
+                for num in range(len(response['value'])):
+                    if 'id' in response['value'][num]:
+                        response['value'][num]['memberOf'] = []
+                        url = 'https://graph.microsoft.com/v1.0/users/{user_id}/memberOf?$count=true&'.format(
+                            user_id=response['value'][num]['id'])
+                        user_response = requests.get(url, headers=header).json()
+
+                        while user_response:
+                            # add to all users above
+                            response['value'][num]['memberOf'] += user_response['value']
+                            # ask for next request if the request has nextlink in return.
+                            # if there was no else, it will return none in the next while loop and exit
+                            user_response = requests.get(user_response['@odata.nextLink'],
+                                                         headers=header).json() if '@odata.nextLink' in \
+                                                                                   user_response else None
+        return response
+
+    def get_url(self, url, header=None):
+        header = self._set_header(header)
+
+        # Make initial request
+        response = requests.get(url, headers=header).json()
+        return response
+
+    def get_group(self, displayname, exact_match=True, header=None):
+        url = 'https://graph.microsoft.com/v1.0/groups?$count=true&$search=%22displayName:{name}%22'.format(
+            name=displayname.replace(" ", '%20'))
+
+        header = self._set_header(header)
+        # Required for this url
+        header['ConsistencyLevel'] = 'Eventual'
+
+        # Make initial request
+        response = requests.get(url, headers=header).json()
+
+        # Grab members
+        if 'value' in response:
+            # check if its EXACT value
+            if exact_match:
+                found_flag = False
+                for item in response['value']:
+                    if item['displayName'].lower() == displayname.lower():
+                        found_flag = True
+                        response['value'] = [item]
+                        break
+                if not found_flag:
+                    response['value'] = []
+                    return response
+
+            # Go through all results
+            for num in range(len(response['value'])):
+                if 'id' in response['value'][num]:
+                    response['value'][num]['members'] = []
+                    url = 'https://graph.microsoft.com/v1.0/groups/{group_id}/members?$count=true&'.format(
+                        group_id=response['value'][num]['id'])
+                    group_response = requests.get(url, headers=header).json()
+
+                    while group_response:
+                        # add to all users above
+                        response['value'][num]['members'] += group_response['value']
+                        # ask for next request if the request has nextlink in return.
+                        # if there was no else, it will return none in the next while loop and exit
+                        group_response = requests.get(group_response['@odata.nextLink'],
+                                                      headers=header).json() if '@odata.nextLink' in \
+                                                                                group_response else None
+
+        return response
--- README.md
+++ README.md
@@ -1,2 +1,64 @@
 # Sarkis-Tools-Azure-Wrapper
-Makes authenticating and API calls to azure a piece of cake
+I created the module to easily contact the Azure AD table and grab user and group data. It was always a pain trying to find the extra headers and right urls. This module will take the pain out for you.
+
+# Requirements
+pip install msal
+
+pip install requests
+
+Create an app in your azure tenant and give access to read the directory objects.
+
+# Usage steps
+1: Create the object and authenticate
+```python
+import Azure
+AD = Azure(TenantID, Client, SecretValue)
+AD.auth()
+```
+By default you get these user fields but feel free to change them. set this to None if you want default fields to be returned instead.
+```python
+AD.user_fields = '&$select=accountEnabled,ageGroup,assignedLicenses,assignedPlans,assignedPlans,' \
+                           'assignedPlans,businessPhones,city,companyName,consentProvidedForMinor,country,' \
+                           'createdDateTime,createdObjects,creationType,department,displayName,employeeHireDate,' \
+                           'employeeId,employeeOrgData,costCenter,employeeOrgData,division,employeeType,' \
+                           'externalUserState,faxNumber,givenName,identities,imAddresses,isResourceAccount,' \
+                           'jobTitle,mail,mailNickname,manager,id,mobilePhone,officeLocation,' \
+                           'onPremisesExtensionAttributes,extensionAttribute,onPremisesImmutableId,' \
+                           'onPremisesLastSyncDateTime,onPremisesProvisioningErrors,onPremisesProvisioningErrors,' \
+                           'onPremisesSamAccountName,onPremisesSecurityIdentifier,onPremisesSyncEnabled,otherMails,' \
+                           'passwordPolicies,passwordProfile,forceChangePasswordNextSignIn,passwordProfile,' \
+                           'forceChangePasswordNextSignInWithMfa,postalCode,preferredLanguage,provisionedPlans,' \
+                           'provisionedPlans,proxyAddresses,showInAddressList,state,streetAddress,surname,' \
+                           'usageLocation,userPrincipalName,userType,region'
+```
+
+
+2: Ask for a user. exact_match to get exact matches, value=value. memberof=True will do a second call to grab the memberOf fields for that user. 
+```python
+user = AD.get_displayname('Sarkis Fesliyan')
+```
+OR
+```python
+user = AD.get_username_new('sfesliyan', exact_match=False, memberof=True)
+```
+3: Find a group. Choose exact match or all results
+```python
+group = AD.get_group('okta-linux-users', exact_match=True)
+```
+4: Get a custom url endpoint if its something you need to tailer to your own needs.
+```python
+custom_endpoint = AD.get_url('https://graph.microsoft.com/v1.0/users/{user_id}/memberOf?$count=true')
+```
+5: Get all users in your environment. Ask for disabled user if you want.
+```python
+all_users = AD.get_all_users(disabled_users=False)
+```
+
+6: Get all disabled users
+```python
+disabled_users = AD.get_all_disabled_users()
+```
+7: Get all groups in your environement
+```python
+all_groups = AD.get_all_groups()
+```
